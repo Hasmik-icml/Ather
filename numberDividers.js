9faa73098979b2afbc22cbea9ac1e06e4ca91ae5
Hasmik-icml <gevorgyan.icml@gmail.com>
2021-06-23 14:30:33 +0400

    numberDividers

---
+++ numberDividers.js
@@ -0,0 +1,11 @@
+function numberDividers(num) {
+  let array = [];
+  for (let i = 2; i <= num; ++i ) {
+    if (num % i == 0) {
+      array.push(i);
+    }
+  }
+  return array;
+}
+console.log(numberDividers(100));
+
